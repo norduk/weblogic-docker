e1e80113dfbbb62f5d8d68fc1b9d40c6c7ce1e29
Valera <thedatahord@yahoo.co.jp>
2023-04-01 00:12:29 +0300

    fix LOCA_JVM alpine

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 echo "Generating silent xml..."
  
 BEA_HOME="/opt/hcfb/weblogic/Oracle/Middleware/"
-LOCAL_JVMS="/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.261-2.6.22.2.el7_8.x86_64"
+LOCAL_JVMS="/usr/lib/jvm/java-1.7-openjdk/"
 WLS_DIR="${BEA_HOME}wlserver_10.3"
 OCP_DIR="${BEA_HOME}coherence_3.7"
  
 JVER=`echo $LOCAL_JVMS | awk -F '/' '{print $4}'`
  
-AP_PORT=("7001")
-AS_PORT=("7002")
+AP_PORT="7001"
+AS_PORT="7002"
  
 
-SERVER=("IBS-RT")
-DOMAIN=("RT")
-MACHINE=("UNIX-6885")
+SERVER="IBS-RT"
+DOMAIN="RT"
+MACHINE="UNIX-6885"
  
 
 XML="<?xml version=\"1.0\" encoding=\"UTF-8\"?><bea-installer><input-fields><data-value name=\"BEAHOME\" value=\"${BEA_HOME}\" /><data-value name=\"WLS_INSTALL_DIR\" value=\"${WLS_DIR}\" /><data-value name=\"OCP_INSTALL_DIR\" value=\"${OCP_DIR}\" /><data-value name=\"OCM_SUPPORT_EMAIL\" value=\"no\" /><data-value name=\"OCM_SUPPORT_PASSWORD\" value=\"no\" /><data-value name=\"COMPONENT_PATHS\" value=\"WebLogic Server/Core Application Server|WebLogic Server/Administration Console|Oracle Coherence/Coherence Product Files|WebLogic Server/Configuration Wizard and Upgrade Framework|WebLogic Server/Web 2.0 HTTP Pub-Sub Server|WebLogic Server/WebLogic SCA|WebLogic Server/WebLogic JDBC Drivers|WebLogic Server/Third Party JDBC Drivers|WebLogic Server/WebLogic Server Clients|WebLogic Server/WebLogic Web Server Plugins|WebLogic Server/UDDI and Xquery Support\" /><data-value name=\"LOCAL_JVMS\" value=\"$LOCAL_JVMS\"/></input-fields></bea-installer>"
